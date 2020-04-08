c864e461701a5836fc3cf4ae2be780a66bd014b6
Michael Jenkins <michaeljenkins@michaels-mbp-4.myfiosgateway.com>
2020-04-08 13:54:36 -0400

    new changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import *  as bs from "react-bootstrap";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Header from './header'
+import Campaign from './campaigns'
+import Prediction from './prediction'
+import Home from './home'
 
-function App() {
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <bs.Container fluid className="p-0 min-vh-100 d-flex flex-column" style={{color:"black"}}>
+        {/* Header Navigation*/}
+        <bs.Row noGutters className="flex-grow-0 flex-shrink-0 shadow-sm">
+          <bs.Col md="12">
+             <Header />
+          </bs.Col>
+        </bs.Row>
+
+
+          
+          {/* MidRow*/}
+      <bs.Row noGutters className="flex-grow-1">
+          {/* Main Page Content*/}
+          <bs.Col className="MainSection" md="12">
+            <Switch>
+              <Route path="/campaigns">
+                   <Campaign /> 
+              </Route>
+              <Route path="/prediction">
+                 <Prediction /> 
+              </Route>
+              <Route path="/">
+                { <Home /> }
+              </Route>
+            </Switch>
+          </bs.Col>
+          
+        </bs.Row>
+  
+  {/* Footer*/}
+        <bs.Row>
+          {/* <Footer /> */}
+          Footer
+        </bs.Row>
+      </bs.Container>
+    </Router>
   );
 }
-
-export default App;
